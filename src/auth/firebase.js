565d3ebfa96a1a6bb90168e355e0f70e1a6633b8
Cosmi27 <percic.cosmi27@gmail.com>
2024-05-20 11:28:14 +0300

    wip

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -5,16 +5,15 @@ import { getAuth } from "firebase/auth";
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = { 
-    apiKey: "AIzaSyAsCpFj6lU2Vu338ruOfdMs_Nfz6QrCEsE",
-    authDomain: "cosmina-119b5.firebaseapp.com",
-    projectId: "cosmina-119b5",
-    storageBucket: "cosmina-119b5.appspot.com",
-    messagingSenderId: "91507145123",
-    appId: "1:91507145123:web:d1c5bb9f0f1d463b96c216"
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCKpOH_zIjKdTeTnzOhKP-Z0tsGFBBONtg",
+    authDomain: "movie-picks1.firebaseapp.com",
+    projectId: "movie-picks1",
+    storageBucket: "movie-picks1.appspot.com",
+    messagingSenderId: "653549990495",
+    appId: "1:653549990495:web:b170b00fd9b52cae7e61ea",
+    measurementId: "G-ENMD3RBT3R"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
