a2bc4f26446e56e2d13c5d174b731fbeace2c9ff
Gal Alexandra <alexandra.gabriela.gal@gmail.com>
2024-05-20 11:40:32 +0300

    .

--- src/layout/MovieCard.jsx
+++ src/layout/MovieCard.jsx
@@ -41,16 +41,15 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
         alt="movie-card"
-      />
-      <div className="flex items-center text-center p-1 text-white">
-        <h5>{title}</h5>
-        {currentUser && (
+        />
+    <div className="flex items-center justify-between text-left p-1 text-white">
+      <h5>{title}</h5>
+      {currentUser && (
           <span className={`tag ${getVoteClass(vote_average)}`}>
-            {vote_average.toFixed(1)}
+              {vote_average.toFixed(1)}
           </span>
-        )}
-        
-      </div>
+      )}
+  </div>
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-black bg-opacity-75 opacity-0 transition-opacity duration-300 hover:opacity-100 overflow-y-auto p-4">
         <h2>Overview</h2>
         <p>{overview}</p>
